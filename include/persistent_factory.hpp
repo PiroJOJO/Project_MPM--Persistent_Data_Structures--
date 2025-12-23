6226bf3febc9f45ff79f72bfc5a13c6217bec573
filbertmarina <112921911+filbertmarina@users.noreply.github.com>
2025-12-23 14:11:23 +0700

    Update persistent_factory.hpp
    Deleted unessesary couts

--- include/persistent_factory.hpp
+++ include/persistent_factory.hpp
@@ -15,7 +15,6 @@ public:
     template<typename T>
     static PersistentVector<T> listToVector(const PersistentList<T>& list) {
         PersistentVector<T> result;
-        std::cout << "DEBUG: listToVector called, list size = " << list.size() << std::endl;
 
         // begin()/end()
         try {
@@ -43,7 +42,6 @@ public:
     template<typename T>
     static PersistentList<T> vectorToList(const PersistentVector<T>& vector) {
         PersistentList<T> result;
-        std::cout << "DEBUG: vectorToList called, vector size = " << vector.size() << std::endl;
 
         for (size_t i = vector.size(); i > 0; --i) {
             try {
@@ -70,7 +68,6 @@ public:
     template<typename K, typename V>
     static PersistentVector<std::pair<K, V>> mapToVector(const PersistentMap<K, V>& map) {
         PersistentVector<std::pair<K, V>> result;
-        std::cout << "DEBUG: mapToVector called" << std::endl;
 
         // итераторы
         try {
@@ -93,7 +90,6 @@ public:
     template<typename K, typename V>
     static PersistentList<std::pair<K, V>> mapToList(const PersistentMap<K, V>& map) {
         PersistentList<std::pair<K, V>> result;
-        std::cout << "DEBUG: mapToList called" << std::endl;
 
         // в вектор, затем в список
         auto vec = mapToVector(map);
@@ -106,7 +102,6 @@ public:
     template<typename K, typename V>
     static PersistentMap<K, V> vectorToMap(const std::vector<std::pair<K, V>>& vec) {
         PersistentMap<K, V> result;
-        std::cout << "DEBUG: vectorToMap called, vector size = " << vec.size() << std::endl;
 
         for (const auto& pair : vec) {
             result = result.set(pair.first, pair.second);
@@ -121,7 +116,6 @@ public:
     template<typename K, typename V>
     static PersistentMap<K, V> persistentVectorToMap(const PersistentVector<std::pair<K, V>>& vec) {
         PersistentMap<K, V> result;
-        std::cout << "DEBUG: persistentVectorToMap called, vector size = " << vec.size() << std::endl;
         // PersistentVector -> std::vector -> vectorToMap
         std::vector<std::pair<K, V>> temp;
 
@@ -154,5 +148,6 @@ public:
         return vectorToMap(temp);
     }
 };
+
 
 #endif 
